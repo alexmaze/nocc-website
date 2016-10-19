fec32f5b07bf011f11e4b36dee648b575ac35bda
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2016-10-19 15:07:53 +0800

    add some libs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
+import { sync } from 'vuex-router-sync'
 import App from './App'
+import store from './store'
 import router from './router'
+
+require('es6-promise').polyfill()
+require('isomorphic-fetch')
+
+sync(store, router)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(App),
+  store,
   router
 })
