d62edbccf48cfb9b1d395cadba690c352fecf89e
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2016-10-30 16:32:54 +0800

    finsih showcase

--- src/app/showcase/index.js
+++ src/app/showcase/index.js
@@ -1,10 +1,50 @@
 import './showcase.less'
+import { Slider, SliderItem } from 'vue-easy-slider'
+
 export default {
   name: 'showcase',
   template: require('./showcase.html'),
+  components: { Slider, SliderItem },
   data () {
     return {
-      msg: ''
+      slides: [{
+        title: 'LI HEN TAIWAN COMTENPORARY JEWELRY LECTURE',
+        url: '/static/demo/case1.png',
+        date: '2016.5.1-5.3'
+      }, {
+        title: 'LI HEN TAIWAN COMTENPORARY JEWELRY LECTURE',
+        url: '/static/demo/case2.png',
+        date: '2016.5.1-5.3'
+      }, {
+        title: 'LI HEN TAIWAN COMTENPORARY JEWELRY LECTURE',
+        url: '/static/demo/case3.png',
+        date: '2016.5.1-5.3'
+      }],
+      news: [{
+        title: 'LI HEN TAIWAN COMTENPORARY JEWELRY LECTURE',
+        url: '/static/demo/case2.png',
+        date: '2016.5.1-5.3'
+      }, {
+        title: 'LI HEN TAIWAN COMTENPORARY JEWELRY LECTURE',
+        url: '/static/demo/case3.png',
+        date: '2016.5.1-5.3'
+      }, {
+        title: 'LI HEN TAIWAN COMTENPORARY JEWELRY LECTURE',
+        url: '/static/demo/case4.png',
+        date: '2016.5.1-5.3'
+      }, {
+        title: 'LI HEN TAIWAN COMTENPORARY JEWELRY LECTURE',
+        url: '/static/demo/case5.png',
+        date: '2016.5.1-5.3'
+      }, {
+        title: 'LI HEN TAIWAN COMTENPORARY JEWELRY LECTURE',
+        url: '/static/demo/case6.png',
+        date: '2016.5.1-5.3'
+      }, {
+        title: 'LI HEN TAIWAN COMTENPORARY JEWELRY LECTURE',
+        url: '/static/demo/case7.png',
+        date: '2016.5.1-5.3'
+      }]
     }
   }
 }
