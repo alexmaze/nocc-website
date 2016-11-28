5754d2092af7ed114254b49fcc0fb6406f305b11
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2016-11-28 23:40:26 +0800

    finish faculty

--- src/app/faculty/index.js
+++ src/app/faculty/index.js
@@ -7,38 +7,7 @@ export default {
   components: { UserCard },
   data () {
     return {
-      userList: [{
-        id: '1',
-        url: '/static/demo/event1.png',
-        position: 'Chairman',
-        email: 'charldd@alex.com',
-        title: 'Prof. Lou Yongqi',
-        description: 'The Spacecraft Tracking and Data (Acquisition) Network (STADAN or STDN) was established by NASA to satisfy the requirement for long-duration, highly-available space to ground commons. Real-time operational control and scheduling of the network was provided by the Network Operations Control Center (NOCC) at the Goddard Space Flight Center (GSFC) in Greenbelt, Maryland.[1]'
-      }, {
-        id: '2',
-        url: '/static/demo/event5.png',
-        position: 'Chairman',
-        title: 'Prof. Lou Yongqi',
-        description: 'The Spacecraft Tracking and Data (Acquisition) Network (STADAN or STDN) was established by NASA to satisfy the requirement for long-duration, highly-available space to ground commons. Real-time operational control and scheduling of the network was provided by the Network Operations Control Center (NOCC) at the Goddard Space Flight Center (GSFC) in Greenbelt, Maryland.[1]'
-      }, {
-        id: '2',
-        url: '/static/demo/event5.png',
-        position: 'Chairman',
-        title: 'Prof. Lou Yongqi',
-        description: 'The Spacecraft Tracking and Data (Acquisition) Network (STADAN or STDN) was established by NASA to satisfy the requirement for long-duration, highly-available space to ground commons. Real-time operational control and scheduling of the network was provided by the Network Operations Control Center (NOCC) at the Goddard Space Flight Center (GSFC) in Greenbelt, Maryland.[1]'
-      }, {
-        id: '2',
-        url: '/static/demo/event5.png',
-        position: 'Chairman',
-        title: 'Prof. Lou Yongqi',
-        description: 'The Spacecraft Tracking and Data (Acquisition) Network (STADAN or STDN) was established by NASA to satisfy the requirement for long-duration, highly-available space to ground commons. Real-time operational control and scheduling of the network was provided by the Network Operations Control Center (NOCC) at the Goddard Space Flight Center (GSFC) in Greenbelt, Maryland.[1]'
-      }, {
-        id: '2',
-        url: '/static/demo/event5.png',
-        position: 'Chairman',
-        title: 'Prof. Lou Yongqi',
-        description: 'The Spacecraft Tracking and Data (Acquisition) Network (STADAN or STDN) was established by NASA to satisfy the requirement for long-duration, highly-available space to ground commons. Real-time operational control and scheduling of the network was provided by the Network Operations Control Center (NOCC) at the Goddard Space Flight Center (GSFC) in Greenbelt, Maryland.[1]'
-      }]
+      userList: []
     }
   },
   created () {
@@ -46,14 +15,29 @@ export default {
   },
   methods: {
     loadData () {
-      console.log('hello events')
-      console.log(this.$route.params.type)
+      fetch(`/api/faculty`)
+      .then(response => {
+        if (response.status >= 400) {
+          throw new Error('Bad response from server')
+        }
+        return response.json()
+      })
+      .then(data => {
+        this.userList = []
+        data.forEach(item => {
+          item.description = delHtmlTag(item.description)
+          item.description_en = delHtmlTag(item.description_en)
+          this.userList.push(item)
+        })
+      })
     }
   },
-  watch: {
-    '$route' (to, from) {
-      // 对路由变化作出响应...
-      this.loadData()
-    }
+  mounted () {
+    console.log('faculty mounted')
+    this.loadData()
   }
+}
+
+function delHtmlTag (str) {
+  return str.replace(/<[^>]+>/g, '')
 }
